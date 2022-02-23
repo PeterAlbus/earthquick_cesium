6b8e994f925a69e931d9aad47dd07172ba8bd7f6
PeterAlbus <wuhongdb@163.com>
2022-02-23 21:17:39 +0800

    添加时间选择器

--- src/components/EarthquakeSelect.vue
+++ src/components/EarthquakeSelect.vue
@@ -7,6 +7,23 @@
       style="z-index: 9999"
   >
     <el-input v-model="searchEarthquake" placeholder="输入地震名称搜索" />
+    <el-collapse>
+      <el-collapse-item title="条件筛选">
+        <el-form>
+          <el-form-item label="时间范围">
+            <el-date-picker
+                v-model="timeRange"
+                :shortcuts="shortcuts"
+                type="datetimerange"
+                range-separator="到"
+                start-placeholder="最早日期"
+                end-placeholder="最晚日期"
+            >
+            </el-date-picker>
+          </el-form-item>
+        </el-form>
+      </el-collapse-item>
+    </el-collapse>
     <div class="el-dialog-div">
       <el-descriptions
           :title="item.earthquakeName"
@@ -68,6 +85,66 @@ export default {
     return {
       earthquakeSelectVisible:false,
       searchEarthquake: "",
+      timeRange:[
+        new Date(2000, 10, 10, 10, 10),
+        new Date(),
+      ],
+      shortcuts:[
+        {
+          text: '最近一天',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24)
+            return [start, end]
+          }
+        },
+        {
+          text: '最近一周',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            return [start, end]
+          }
+        },
+        {
+          text: '最近一个月',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            return [start, end]
+          }
+        },
+        {
+          text: '最近3个月',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            return [start, end]
+          }
+        },
+        {
+          text: '最近半年',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 180)
+            return [start, end]
+          }
+        },
+        {
+          text: '最近一年',
+          value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365)
+            return [start, end]
+          }
+        }
+      ]
     }
   },
   methods:{
@@ -105,7 +182,7 @@ export default {
 
 <style scoped>
 .el-dialog-div{
-  height: 50vh;
+  height: 40vh;
   overflow: auto;
   padding: 10px;
 }
